bf0e0b3a39a03702cbda713b2bfd76985d72e845
Alex <Alex.Ciprian.Lopez@logirail.com>
2023-08-30 11:20:07 +0200

    Initial Commit

---
+++ ZaraRickYMorty/ZaraRickYMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZaraRickYMortyApp.swift
+//  ZaraRickYMorty
+//
+//  Created by Alex Ciprián López on 30/8/23.
+//
+
+import SwiftUI
+
+@main
+struct ZaraRickYMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
