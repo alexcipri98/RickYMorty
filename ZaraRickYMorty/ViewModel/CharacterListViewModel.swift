2d4b33a5183a2ed978083168f4b43bfddfb94fdc
Alex <Alex.Ciprian.Lopez@logirail.com>
2023-08-30 12:00:32 +0200

    CharacterListViewModel edited

--- ZaraRickYMorty/ViewModel/CharacterListViewModel.swift
+++ ZaraRickYMorty/ViewModel/CharacterListViewModel.swift
@@ -5,16 +5,21 @@
 //  Created by Alex Ciprián López on 30/8/23.
 //
 
+import Foundation
 import SwiftUI
 
-struct CharacterListViewModel: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+class CharacterListViewModel: ObservableObject {
+    @Published var characters: [Character] = []
+    @Published var isLoading: Bool = true
+    private let characterService = CharacterService()
 
-struct CharacterListViewModel_Previews: PreviewProvider {
-    static var previews: some View {
-        CharacterListViewModel()
+    func fetchCharacters() {
+        isLoading = true
+        characterService.fetchCharacters { [weak self] characters in
+            DispatchQueue.main.async {
+                self?.characters = characters
+                self?.isLoading = false
+            }
+        }
     }
 }
